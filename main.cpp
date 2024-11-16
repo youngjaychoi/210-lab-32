25c89919ba10448cc6a2dbb844ca9cdd6c63ce1e
youngjaychoi <youngjaychoi02@gmail.com>
2024-11-15 20:29:19 -0800

    commit2

--- main.cpp
+++ main.cpp
@@ -1,2 +1,29 @@
 #include "Car.h"
 #include <iostream>
+#include <deque>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+const int INITIAL_SIZE = 2;
+
+int main() {
+    srand(time(0));
+
+    deque<Car> queue;
+
+    for (int i = 0; i < INITIAL_SIZE; i++) {
+        queue.push_back(Car());
+    }
+
+    cout << "Initial queue:" << endl;
+    
+
+
+
+
+
+
+
+    return 0;
+}
