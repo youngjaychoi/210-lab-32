454f24780d4bae37d7598ea4e5e645d73b7cf20b
youngjaychoi <youngjaychoi02@gmail.com>
2024-11-15 20:39:25 -0800

    commit3

--- main.cpp
+++ main.cpp
@@ -11,13 +11,28 @@ int main() {
     srand(time(0));
 
     deque<Car> queue;
+    int time = 1, randomNum = 0;
 
-    for (int i = 0; i < INITIAL_SIZE; i++) {
+    for (int i = 0; i < INITIAL_SIZE; ++i) {
         queue.push_back(Car());
     }
 
     cout << "Initial queue:" << endl;
+    for (int i = 0; i < queue; ++i) {
+        queue[i].print();
+    }
     
+    while (!queue.empty()) {
+        randomNum = rean() % 100  + 1;
+
+        cout << "Time: " << time << " Operation: ";
+        if (randomNum <= 55) {
+            cout << "Car paid: ";
+            queue.front().print();
+            queue.pop_front();
+        }
+        }
+    }
 
 
 
